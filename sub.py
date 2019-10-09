92addcfd02fe66ed5336f8a243d2c0e92d2c411b
Kevin Luo <kluo1997@gmail.com>
2019-10-09 12:18:14 -0400

    Add files via upload

--- sub.py
+++ sub.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import re
+import math
 import os
 import pandas as pd
 
@@ -19,10 +20,33 @@ def check_dataset_validity():
                 print("==================\n")
 
 
-def site_sanitizer(fname):
+def timestamp_update(timestamp, hrs):
+    date_add_hrs = datetime.datetime(int(timestamp[0:4]), int(timestamp[4:6]), int(timestamp[6:8]),
+                                     int(timestamp[8:10]), int(timestamp[10:12])) + \
+                   datetime.timedelta(hours=hrs)
+    new_date = str(date_add_hrs.year) + str(date_add_hrs.month).zfill(2) + str(date_add_hrs.day).zfill(2) + \
+               str(date_add_hrs.hour).zfill(2) + \
+               str(date_add_hrs.minute).zfill(2)
+    return new_date
+
+
+def site_sanitizer_aux(fname, sitelistdf):
     # ===========================
     # Issue 1: PPFD not found
     # ===========================
+    # sitelistdf = pd.read_csv("Site-List.csv")
+    # sitelistdf.columns = sitelistdf.columns.str.strip()
+    # sitelistdf['Site Id'] = sitelistdf['Site Id'].str.strip()
+    # sitelistdf_obj = sitelistdf.select_dtypes(['object'])
+    # sitelistdf[sitelistdf_obj.columns] = sitelistdf_obj.apply(lambda x: x.str.strip())
+
+    with open(fname, newline='') as f:
+        reader = csv.reader(f)
+        siteid = next(reader)[0].split()[2]
+
+    siteinfo = sitelistdf.loc[sitelistdf['Site Id'] == siteid]
+    lon = float(siteinfo['Long'])
+    hrs = -math.ceil(((-7.5 - lon) / 15))
 
     df = pd.read_csv(fname, header=2)
     hasvpd = False
@@ -49,16 +73,38 @@ def site_sanitizer(fname):
                         or row['VPD_PI'] == -9999.0:
                     df.drop(index, inplace=True)
                 else:
+                    # Calculate VPD_PI
                     if not hasvpd:
                         svp = 610.7 * 10 ** ((7.5 * row['TA']) / (237.3 + row['TA'])) * 0.01
                         vpd = ((100 - row['RH']) / 100) * svp
                         df.at[index, 'VPD_PI'] = vpd
+                    # Adjust timestamp to UTC
+                    new_start_datetime = timestamp_update(str(row['TIMESTAMP_START']), hrs)
+                    new_end_datetime = timestamp_update(str(row['TIMESTAMP_END']), hrs)
+                    df.at[index, 'TIMESTAMP_START'] = new_start_datetime
+                    df.at[index, 'TIMESTAMP_END'] = new_end_datetime
             else:
                 df.drop(index, inplace=True)
-        df.drop('RH', axis=1)
+        df.drop('RH', axis=1, inplace=True)
         new_fname = "Clean_" + fname
         df.to_csv(new_fname, index=False)
 
 
+def site_sanitizer():
+    # Obtain a list of information of sites such as longitude and latitude
+    sitelistdf = pd.read_csv("Site-List.csv")
+    sitelistdf.columns = sitelistdf.columns.str.strip()
+    sitelistdf['Site Id'] = sitelistdf['Site Id'].str.strip()
+
+    directory = os.fsencode("/Users/kaiweiluo/PycharmProjects/Machine-Learning-NASA/Site-CSV/WET")
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".csv"):
+            file_path = "/Users/kaiweiluo/PycharmProjects/Machine-Learning-NASA/Site-CSV/WET/" + filename
+            print(filename)
+            site_sanitizer_aux(file_path, sitelistdf)
+
+
 # check_dataset_validity()
-site_sanitizer("AMF_US-OWC_BASE_HH_2-5.csv")
+# site_sanitizer_aux("AMF_US-OWC_BASE_HH_2-5.csv")
+site_sanitizer()
