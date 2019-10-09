b9e7dad22d359663396654e69f84d880f415d0dd
Kevin Luo <kluo1997@gmail.com>
2019-10-09 01:52:35 -0400

    Add files via upload

--- sub.py
+++ sub.py
@@ -1,38 +1,64 @@
 import csv
 import datetime
+import re
+import os
 import pandas as pd
 
 
-def check_dataset_validity(fname):
-    df = pd.read_csv(fname, header=0)
-    hasGpp = 0
-    for index, row in df.iterrows():
-        site_path = "/Users/kaiweiluo/PycharmProjects/Machine-Learning-NASA/Site_Data_RAW_Collection/" \
-                    + row['Site_Name']
-        df2 = pd.read_csv(site_path, header=2)
-        print("==================")
-        print(row['Site_Name'])
-        if 'GPP_PI_F' in df2.columns:
-            print("Has GPP: True")
-            hasGpp += 1
-        else:
-            print("Has GPP: False")
-        print("==================\n")
-
-    print("Number of total sites: " + str(len(df.index)))
-    print("Number of sites has GPP: " + str(hasGpp))
+def check_dataset_validity():
+    directory = os.fsencode("/Users/kaiweiluo/PycharmProjects/Machine-Learning-NASA/Site-CSV/WET")
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".csv"):
+            file_path = "/Users/kaiweiluo/PycharmProjects/Machine-Learning-NASA/Site-CSV/WET/" + filename
+            df = pd.read_csv(file_path, header=2)
+            if 'GPP_PI_F' in df.columns and 'PPFD_IN' in df.columns:
+                print("==================")
+                print(filename)
+                print("Has GPP and PPFD_IN: True")
+                print("==================\n")
 
 
-def clean_site_date(fname):
+def site_sanitizer(fname):
+    # ===========================
+    # Issue 1: PPFD not found
+    # ===========================
+
     df = pd.read_csv(fname, header=2)
-    df = df[['TIMESTAMP_START', 'TIMESTAMP_END', 'TA', 'PPFD_IN']]
-    for index, row in df.iterrows():
-        if row['TA'] == -9999.0 or row['PPFD_IN'] == -9999.0:
-            df.drop(index, inplace=True)
-            print(index)
-    new_fname = "Clean_" + fname
-    df.to_csv(new_fname, index=False)
+    hasvpd = False
+    hasppfd = False
+    hasgpp = False
+    rgpp = re.compile("[gG][pP][pP]")
+    rppfd = re.compile("^[pP][pP][fF][dD]_[iI][nN]$")
+    rvpd = re.compile("^[vV][pP][dD]_[pP][iI]$")
+    cols = list(df.columns)
+    for col in cols:
+        if re.search(rgpp, col):
+            hasgpp = True
+        if re.search(rppfd, col):
+            hasppfd = True
+        if re.search(rvpd, col):
+            hasvpd = True
+    if hasgpp and hasppfd:
+        if not hasvpd:
+            df["VPD_PI"] = ""
+        df = df[['TIMESTAMP_START', 'TIMESTAMP_END', 'TA', 'PPFD_IN', 'VPD_PI', 'GPP_PI_F', 'RH']]
+        for index, row in df.iterrows():
+            if int(row['TIMESTAMP_START']) >= 201501010000:  # Check timestamp validity, must be later than 2015
+                if row['TA'] == -9999.0 or row['PPFD_IN'] == -9999.0 or row['GPP_PI_F'] == -9999.0 \
+                        or row['VPD_PI'] == -9999.0:
+                    df.drop(index, inplace=True)
+                else:
+                    if not hasvpd:
+                        svp = 610.7 * 10 ** ((7.5 * row['TA']) / (237.3 + row['TA'])) * 0.01
+                        vpd = ((100 - row['RH']) / 100) * svp
+                        df.at[index, 'VPD_PI'] = vpd
+            else:
+                df.drop(index, inplace=True)
+        df.drop('RH', axis=1)
+        new_fname = "Clean_" + fname
+        df.to_csv(new_fname, index=False)
 
 
-# clean_site_date("AMF_US-A32_BASE_HH_1-5.csv")
-check_dataset_validity("Site_Name.csv")
+# check_dataset_validity()
+site_sanitizer("AMF_US-OWC_BASE_HH_2-5.csv")
