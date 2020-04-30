63713bfe1a17d840e3f6af7c4248a7263de6b47d
Kevin Luo <kluo1997@gmail.com>
2020-04-29 23:50:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -49,38 +49,22 @@ def site_sanitizer_aux(fname, epic_prefix, sitelistdf, outputpath):
     # Issue 2: GPP_PI_F_1_2, Multiple name, OWN
     # ===========================
 
-    with open(fname, newline='') as f:
-        reader = csv.reader(f)
-        siteid = next(reader)[0].split()[2]
+    siteid = fname.split('/')[-1].split('_')[1]
 
-    siteinfo = sitelistdf.loc[sitelistdf['Site Id'] == siteid]
-    lon = float(siteinfo['Long'])
+    siteinfo = sitelistdf.loc[sitelistdf['SiteId'] == siteid]
+    lon = float(siteinfo['Longitude'])
     hrs = -math.ceil(((-7.5 - lon) / 15))
     epic_file = epic_prefix + siteid + ".csv"
 
-    df = pd.read_csv(fname, header=2)
+    df = pd.read_csv(fname)
     epicdf = pd.read_csv(epic_file, header=None)
     epicdf.columns = ["Year", "Month", "Day", "Hour", "Minute", "Second", "Blue", "Red", "NIRS"]
-    hasvpd = False
-    hasppfd = False
-    hasgpp = False
-    rgpp = re.compile("[gG][pP][pP]")
-    rppfd = re.compile("^[pP][pP][fF][dD]_[iI][nN]$")
-    rvpd = re.compile("^[vV][pP][dD]_[pP][iI]$")
-    cols = list(df.columns)
-    for col in cols:
-        if re.search(rgpp, col):
-            hasgpp = True
-        if re.search(rppfd, col):
-            hasppfd = True
-        if re.search(rvpd, col):
-            hasvpd = True
 
     # Sanitizing Epic dataset
     nirv_list = []
     epic_date_list = []
     for index, row in epicdf.iterrows():
-        print("EPIC " + siteid + " " + str(index))
+        print("I. EPIC " + siteid + " " + str(index))
         if row["Year"] == 0 or row["Month"] == 0 or row["Day"] == 0 or row["Hour"] == 0 or row["Minute"] == 0 or row[
                 "Second"] == 0 or row["Blue"] == 0 or row["Red"] == 0 or row["NIRS"] == 0:
             epicdf.drop(index, inplace=True)
@@ -95,31 +79,22 @@ def site_sanitizer_aux(fname, epic_prefix, sitelistdf, outputpath):
     epicdf['NIRV'] = nirv_list
     epicdf['Datetime'] = epic_date_list
 
-    if hasgpp and hasppfd:
-        if not hasvpd:
-            df["VPD_PI"] = ""
-        df = df[['TIMESTAMP_START', 'TIMESTAMP_END', 'TA', 'PPFD_IN', 'VPD_PI', 'GPP_PI_F', 'RH']]
-        for index, row in df.iterrows():
-            print("Site " + siteid + " " + str(index))
-            if int(row['TIMESTAMP_START']) >= 201501010000:  # Check timestamp validity, must be later than 2015
-                if row['TA'] == -9999.0 or row['PPFD_IN'] == -9999.0 or row['GPP_PI_F'] == -9999.0 \
-                        or row['VPD_PI'] == -9999.0:
-                    df.drop(index, inplace=True)
-                else:
-                    # Adjust timestamp to UTC
-                    new_start_datetime = timestamp_update(str(row['TIMESTAMP_START']), hrs)
-                    new_end_datetime = timestamp_update(str(row['TIMESTAMP_END']), hrs)
-                    df.at[index, 'TIMESTAMP_START'] = new_start_datetime
-                    df.at[index, 'TIMESTAMP_END'] = new_end_datetime
-
-                    # Calculate VPD_PI
-                    if not hasvpd:
-                        svp = 610.7 * 10 ** ((7.5 * row['TA']) / (237.3 + row['TA'])) * 0.01
-                        vpd = ((100 - row['RH']) / 100) * svp
-                        df.at[index, 'VPD_PI'] = vpd
-            else:
+    df = df[['TIMESTAMP_START', 'TIMESTAMP_END', 'TA_F_MDS', 'PPFD_IN', 'VPD_F', 'GPP_NT_VUT_MEAN', 'GPP_NT_CUT_MEAN', 'GPP_DT_VUT_MEAN', 'GPP_DT_CUT_MEAN']]
+    for index, row in df.iterrows():
+        print("II. Site " + siteid + " " + str(index))
+        if int(row['TIMESTAMP_START']) >= 201501010000:  # Check timestamp validity, must be later than 2015
+            if row['TA_F_MDS'] == -9999.0 or row['PPFD_IN'] == -9999.0 or row['VPD_F'] == -9999.0 \
+                    or row['GPP_NT_VUT_MEAN'] == -9999.0 or row['GPP_NT_CUT_MEAN'] == -9999.0 or row['GPP_DT_VUT_MEAN'] == -9999.0 or row['GPP_DT_CUT_MEAN'] == -9999.0:
                 df.drop(index, inplace=True)
-        df.drop('RH', axis=1, inplace=True)
+            else:
+                # Adjust timestamp to UTC
+                new_start_datetime = timestamp_update(str(row['TIMESTAMP_START']), hrs)
+                new_end_datetime = timestamp_update(str(row['TIMESTAMP_END']), hrs)
+                df.at[index, 'TIMESTAMP_START'] = new_start_datetime
+                df.at[index, 'TIMESTAMP_END'] = new_end_datetime
+
+        else:
+            df.drop(index, inplace=True)
 
         # Merge dataframe
         data = []
@@ -127,6 +102,7 @@ def site_sanitizer_aux(fname, epic_prefix, sitelistdf, outputpath):
         epic_iter = epicdf.iterrows()
         site_row = next(site_iter)
         epic_row = next(epic_iter)
+        print("III. Merging...")
         try:
             while site_row is not None and epic_row is not None:
                 if int(site_row[1]['TIMESTAMP_START']) <= int(epic_row[1]['Datetime']) <= int(site_row[1]['TIMESTAMP_END']):
@@ -141,9 +117,9 @@ def site_sanitizer_aux(fname, epic_prefix, sitelistdf, outputpath):
                     site_row = next(site_iter)
         except Exception as e:
             print(e)
-        mergedf = pd.DataFrame(data, columns=['TIMESTAMP_START', 'TIMESTAMP_END', 'TA', 'PPFD_IN', 'VPD_PI',
-                                              'GPP_PI_F', 'NIRV'])
-        new_fname = "Clean_" + siteid + ".csv"
+        mergedf = pd.DataFrame(data, columns=['TIMESTAMP_START', 'TIMESTAMP_END', 'TA_F_MDS', 'PPFD_IN', 'VPD_F', 'GPP_NT_VUT_MEAN',
+                                              'GPP_NT_CUT_MEAN', 'GPP_DT_VUT_MEAN', 'GPP_DT_CUT_MEAN', 'NIRV'])
+        new_fname = siteid + ".csv"
         mergedf.to_csv(os.path.join(outputpath, new_fname), index=False)
 
 
@@ -151,7 +127,7 @@ def site_sanitizer(inpath, outpath, epicpath):
     # Obtain a list of information of sites such as longitude and latitude
     sitelistdf = pd.read_csv("Site-List.csv")
     sitelistdf.columns = sitelistdf.columns.str.strip()
-    sitelistdf['Site Id'] = sitelistdf['Site Id'].str.strip()
+    sitelistdf['SiteId'] = sitelistdf['SiteId'].str.strip()
 
     directory = os.fsencode(inpath)
     for file in os.listdir(directory):
@@ -218,10 +194,10 @@ def regression_controller(datapath, outputpath):
             random_forest_estimate(filepath, outputpath)
 
 
-inpath = "/Users/kaiweiluo/PycharmProjects/GppRegressor/Site-CSV/WET/"
-outpath = "/Users/kaiweiluo/PycharmProjects/GppRegressor/DATASET-WET"
-epicpath = "/Users/kaiweiluo/PycharmProjects/GppRegressor/epic_data"
-dataset_dir = "/Users/kaiweiluo/PycharmProjects/GppRegressor/DATASET-WET"
-fig_output = "/Users/kaiweiluo/PycharmProjects/GppRegressor/DATASET-WET/"
+inpath = "/Users/kaiweiluo/PycharmProjects/GppRegressor/data/"
+outpath = "/Users/kaiweiluo/PycharmProjects/GppRegressor/new_data/"
+epicpath = "/Users/kaiweiluo/PycharmProjects/GppRegressor/epic_data/"
+dataset_dir = "/Users/kaiweiluo/PycharmProjects/GppRegressor/new_data/"
+fig_output = "/Users/kaiweiluo/PycharmProjects/GppRegressor/results/"
 site_sanitizer(inpath, outpath, epicpath)
-# regression_controller(dataset_dir, fig_output)
+#regression_controller(dataset_dir, fig_output)
